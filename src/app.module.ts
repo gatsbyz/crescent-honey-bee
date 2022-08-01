c393e03beb15ac79d5eb2470cd2d2f3d57f5c4d9
Jesse Lee <jesse@titanvest.com>
2022-08-01 11:04:07 -0400

    .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MessagesModule } from './modules/message/message.module';
 import { RecipientsModule } from './modules/recipient/recipient.module';
+import { config } from 'dotenv';
+config();
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot(`mongodb://localhost/${process.env.DB_NAME}`),
     MessagesModule,
     RecipientsModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,7 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
